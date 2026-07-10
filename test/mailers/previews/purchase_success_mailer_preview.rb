21ee3d294b667f7248d7f95d68600bf9f4b8ad71
Blattsteinn <arnas.lukosius@proton.me>
2026-07-10 19:50:45 +0300

    mailer update

--- test/mailers/previews/purchase_success_mailer_preview.rb
+++ test/mailers/previews/purchase_success_mailer_preview.rb
@@ -1,3 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/purchase_success_mailer
 class PurchaseSuccessMailerPreview < ActionMailer::Preview
+    def successful_purchase
+    order = Order.joins(:order_items).last || Order.new(public_id: "DEMO-XXXX")
+    PurchaseSuccessMailer.successful_purchase(order)
+  end
 end
