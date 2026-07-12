765e4d519a8bbfb475417e86850f5e295e566d24
Blattsteinn <arnas.lukosius@proton.me>
2026-07-12 17:37:55 +0300

    Countries can be seen now

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -11,9 +11,11 @@ class Ahoy::Visit < ApplicationRecord
   def geocode_country!
     return if ip.blank? || ip == "127.0.0.1" || ip == "::1"
 
-    location = Geocoder.search(ip).first
-    update_column(:country, location.country) if location&.country.present?
+    db = MaxMind::DB.new(Rails.root.join("vendor", "GeoLite2-Country.mmdb").to_s)
+    result = db.lookup(ip)
+    update_column(:country, result.dig("country", "iso_code")) if result
   rescue => e
     Rails.logger.warn "Geocode failed for IP #{ip}: #{e.message}"
   end
+
 end
