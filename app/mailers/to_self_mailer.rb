ad2f2b14e617ef2b66404a01c11cfaa2d40dc95c
Blattsteinn <arnas.lukosius@proton.me>
2026-07-14 18:10:27 +0300

    Fxed my mailer

--- app/mailers/to_self_mailer.rb
+++ app/mailers/to_self_mailer.rb
@@ -1,9 +1,9 @@
 class ToSelfMailer < ApplicationMailer
     def mail_self(order)
         @order = order
-        @email = order.email
+        @email = "dokkanriftmanagement@tuta.com"
 
-        @price = convert_from_cents(@order.order_items.first.price)
-        mail(to: "dokkanriftmanagement@tuta.com", subject: "Order received, #{@price} EUR")
+        @price = @order.order_items.first.price / 100.0
+        mail(to: @email, subject: "Order received, #{@price} EUR")
     end
 end
